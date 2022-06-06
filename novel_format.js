f98af43474f4db95519d6b12118e1e5dd4b6d2ec
somenoe <otashi24@gmail.com>
2022-06-06 21:12:48 +0700

    '? …' to '…?'

--- novel_format.js
+++ novel_format.js
@@ -78,7 +78,7 @@ function ellipsis(doc) {
             .replace(/( |\?|\!|\.|,|…|\"|\'|\)|\(|\>)… ([\wé])/g, "$1…$2")
             .replace(/(\w) (…|… )([^\w\s])/g, "$1… $3")
             .replace(/([^\w\s])(…| …) (\w)/g, "$1 …$3")
-            .replace(/([\?\!])(…| …)(^\w)/g, "$1… $3")
+            .replace(/([\?\!])(…| …)([^\w])/g, "…$1 $3")
             .replace(/([A-Z]) … ([a-z])/g, "$1… $2")
             .replace(/([a-z]) … ([A-Z])/g, "$1 …$2")
             .replace(/([a-z]) … ([a-z])/g, "$1… $2")
@@ -404,8 +404,7 @@ function add_child(parent, child) {
 function infinitenoveltranslations() {
     font_styling();
     const parent = document.getElementsByTagName("body")[0];
-    const content_name = "entry-content";
-    const content = document.getElementsByClassName(content_name)[0];
+    const content = document.getElementsByClassName("entry-content")[0];
     const image = content.getElementsByTagName("img")[0];
     const footer = document.getElementsByTagName("footer")[0];
     const a_tags = content.getElementsByTagName("a");
