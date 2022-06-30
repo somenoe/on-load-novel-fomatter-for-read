1f82d0d1b5ecec5488fe438485dd2fcc546dbe82
somenoe <otashi24@gmail.com>
2022-06-30 14:27:34 +0700

    ([^>]+<) to ([^>]*<)

--- novel_format.js
+++ novel_format.js
@@ -50,7 +50,7 @@ function pre_replace
         .replace(/([0-9])([A-Z|a-z])/g, "$1 $2")
         .replace(/『 [0-9] 』/g, "⋆ ")
         // symbol without space
-        .replace(/([’'A-z][A-z]+)([,.!?])([A-z][A-z]*)([^>]+<)/g, "$1$2 $3$4")
+        .replace(/([’'A-z][A-z]+)([,.!?])([A-z][A-z]*)([^>]*<)/g, "$1$2 $3$4")
         // .replace(/([’'A-z][A-z]+)([,.!?])([A-z][A-z]*)/g, "$1$2 $3")
         .replace(/If you are reading this anywhere [^<>]+ com\./g, '')
         ;
@@ -205,10 +205,10 @@ function unreadablize
             .replace(/([「][^\w]+[」])/g, `${unread('$1')}`)
             // unreadablize these symbol
             // .replace(/(\s[^\w][^\w][^\w]+\s)/g, `${unread('$1')}`)
-            .replace(/([†♱*\$#]+)([^>]+<)/g, `${unread('$1')}$2`)
+            .replace(/([†♱*\$#]+)([^>]*<)/g, `${unread('$1')}$2`)
             // only number
             .replace(/(<p>)([0-9\s])(<\/p>)/g, `$1${unread('$2')}$3`)
-            .replace(/([A-z])\/([A-z])([^>]+<)/g, `$1${unread('\/')}$2$3`)
+            .replace(/([A-z])\/([A-z])([^>]*<)/g, `$1${unread('\/')}$2$3`)
             // unreadablize dot near quote symbol
             .replace(/\.([」】）』「【（『])/g, `.${unread('$1')}`)
             .replace(/([」】）』「【（『])\./g, `${unread('$1')}.`)
@@ -275,7 +275,7 @@ function quote_symbol
             .replace(/\"\. /g, '." ')
             // in paragraph quote init
             .replace(/([\s>])\"([^\s<])/g, "$1“$2")
-            .replace(/([^\s>]|[!?.]\s*)\"([\s<])([^>]+<)/g, "$1”$2$3")
+            .replace(/([^\s>]|[!?.]\s*)\"([\s<])([^>]*<)/g, "$1”$2$3")
             .replace(/([\s>])\'([^\s>])/g, "$1‘$2")
             .replace(/([^\s<])\'([\s<])/g, "$1’$2")
 
@@ -321,7 +321,9 @@ function japanese_quote_symbol
 
     // // ! need to change all [Apostrophe] to (') first, otherwise it count as [single_close_quote]
     // mainly use english quote symbol ("")
-    if ((double_close_quote + double_open_quote) > (single_open_quote + single_close_quote)) {
+    let sum_double_quote = double_open_quote + double_close_quote;
+    let sum_single_quote = single_open_quote + single_close_quote;
+    if ((sum_double_quote) > (sum_single_quote) || sum_double_quote + sum_single_quote == 0) {
         doc = doc.replace(/“/g, "「 ").replace(/”/g, " 」");
     } else {
         doc = doc.replace(/‘/g, "「 ").replace(/’/g, " 」");
